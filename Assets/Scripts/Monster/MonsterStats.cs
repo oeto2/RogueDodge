990389de25161f7692aa1a0590602d77e4d83882
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-01-24 17:46:04 +0900

    .

--- Assets/Scripts/Monster/MonsterStats.cs
+++ Assets/Scripts/Monster/MonsterStats.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class MonsterStats : MonoBehaviour
 {
-    public string name;
+    public string monsterName = "";
     public int hp;
     public float atk;
     public float speed;
+    public LayerMask target;
 }
