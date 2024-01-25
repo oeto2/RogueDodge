9142c42165b9087f4bae136d2edb5a3a8b96fcb3
purangi <kaya0728@naver.com>
2024-01-25 15:17:08 +0900

    feat: MapCreator 생성

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -6,4 +6,13 @@ public class Map : MonoBehaviour
 {
     public int mapX = 0;
     public int mapY = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enter");
+        if(other.tag == "Player")
+        {
+            MapCreator.I.CreateNextMap(mapX);
+        }
+    }
 }
--- Assets/Scripts/MapCreator.cs
+++ Assets/Scripts/MapCreator.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class MapCreator : MonoBehaviour
 {
     //TODO: Check basic map's size and make new map's location vector
-    [SerializeField] GameObject[] maps;
+    [SerializeField] List<GameObject> maps;
 
+    public static MapCreator I;
+
+    private void Awake()
+    {
+        I = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateNewMap();
     }
 
     // Update is called once per frame
@@ -19,8 +25,23 @@ public class MapCreator : MonoBehaviour
         
     }
 
-    void CreateNewMap()
+    GameObject CreateNewMap()
     {
+        int rand = Random.Range(0, maps.Count);
 
+        GameObject newMap = Instantiate(maps[rand]);
+
+        return newMap;
+    }
+
+    public void CreateNextMap(int curX)
+    {
+        GameObject newMap = CreateNewMap();
+
+        Map nMap = newMap.GetComponent<Map>();
+
+        int newX = nMap.mapX;
+
+        newMap.transform.position = new Vector3((curX + newX) / 2 + 5, 0, 0);
     }
 }
