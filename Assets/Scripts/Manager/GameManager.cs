6db37b40c5819fbee4f4e7f411dd7930f7f2deb3
oeto2 <oeto2@naver.com>
2024-01-26 21:20:38 +0900

    feat 웨이브 및 스테이지 로직 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public enum GAMESTATE
+{
+    BATTLE, //Fighting monsters
+    CLEAR //Wave Or Stage Clear Or Shopping
+}
+
+public enum WAVETYPE
+{
+    NORMAL,
+    BOSS
+}
+
 public class GameManager : MonoBehaviour
 {
     //Singleton
     public static GameManager Instance = null;
+
+    public GAMESTATE eGameState = GAMESTATE.BATTLE;
+    public WAVETYPE eWaveType = WAVETYPE.NORMAL;
 
     private int gold { get; set; }
     public Transform PlayerTransform;
@@ -18,6 +33,10 @@ public class GameManager : MonoBehaviour
     private int curMonsterNum = 0;
     [SerializeField]
     private int deadMonsterNum = 0;
+
+    public int curWaveNum = 1;
+    public int targetWaveNum = 3;
+    public int stageNum = 1;
 
     //Call event When Clear Wave 
     public event Action WaveClearEvent;
@@ -44,16 +63,20 @@ public class GameManager : MonoBehaviour
     public void Start()
     {
         WaveClearEvent += ResetMonsterData;
+        WaveClearEvent += WaveClear;
+        StageClearEvent += StageClear;
     }
 
     public void CallWaveClearEvent()
     {
         WaveClearEvent?.Invoke();
+        eGameState = GAMESTATE.CLEAR;
     }
 
     public void CallStageClearEvent()
     {
         StageClearEvent?.Invoke();
+        eGameState = GAMESTATE.CLEAR;
     }
 
     //Add Dead Monster 
@@ -69,15 +92,35 @@ public class GameManager : MonoBehaviour
     }
 
     //Add CurMonsterNum
-    public void AddCurMonsterNum(int _num)
-    {
-        curMonsterNum += _num;
-    }
+    public void AddCurMonsterNum(int _num) => curMonsterNum += _num;
 
-    //Call Function when Clear current stage 
+    //Call Function when Dead BossMonster
     public void StageClear()
     {
-        //TO DO
+        eGameState = GAMESTATE.CLEAR;
+        eWaveType = WAVETYPE.NORMAL;
+        stageNum++;
+
+        //Call Stage Clear Event
+        CallStageClearEvent();
+    }
+
+    public void WaveClear()
+    {
+        eGameState = GAMESTATE.CLEAR;
+        deadMonsterNum = 0;
+
+        //TODO: Set CurrentMonsterNum to Map Obj
+        curMonsterNum = 1;
+
+
+        if (curWaveNum < targetWaveNum)
+            curWaveNum++;
+        else
+        {
+            eWaveType = WAVETYPE.BOSS;
+            ResetWave();
+        }
     }
 
     public void ResetMonsterData()
@@ -85,4 +128,12 @@ public class GameManager : MonoBehaviour
         curMonsterNum = 0;
         deadMonsterNum = 0;
     }
+
+    public void ResetWave()
+    {
+        curWaveNum = 1;
+        targetWaveNum = 3;
+    }
+
+    public void StartNextWave() => eGameState = GAMESTATE.BATTLE;
 }
