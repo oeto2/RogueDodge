b388e86992c93bfc26c04ab129f6351d06fd5e1c
purangi <kaya0728@naver.com>
2024-01-26 11:48:36 +0900

    feat: 게임 매니저에 curMap, player 추가해서 player가 map의 자식 오브젝트가 되도록 설정

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,7 +7,7 @@ public class Map : MonoBehaviour
     public int mapX = 0;
     private bool isUsed = false;
 
-    [SerializeField] Vector3[] itemSpawn;
+    public Vector3[] itemSpawn;
     //map1 (-8, -2.4) (-3, 4.6), (3.-2.4), (5, 7.6) (10, 2.5)
     //map2 (-9.6, 15.5) (8, 15.5) (-9, 5.5) (8, 5.5) (14, -2.5)
     //map3 (-12,9.6), (13,9.6) (1, 3.6) (-12, -2.4), (13, -2.4)
@@ -16,7 +16,7 @@ public class Map : MonoBehaviour
     {
         if(other.tag == "Player" && !isUsed)
         {
-            MapCreator.I.CreateNextMap(gameObject);
+            MapCreator.instance.CreateNextMap(gameObject);
             isUsed = true;
         }
     }
