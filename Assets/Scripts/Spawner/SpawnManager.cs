a173d7a42f98396255c336911101cd2c85212dea
hygge31 <yiuter1@hanmail.net>
2024-01-29 11:11:09 +0900

    fix.몬스터 스폰 수정
    spawnManger 추가,

---
+++ Assets/Scripts/Spawner/SpawnManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public int round;
+    int currentRound;
+    public int spawnAmount;
+
+    public List<GameObject> bossMonsters;
+    public List<GameObject> spawnerList;
+    public List<GameObject> spawnMonsters;
+
+
+    public void RunSpawn()
+    {
+        if(round <= currentRound)
+        {
+            int ran = Random.Range(0, bossMonsters.Count);
+            Instantiate(bossMonsters[ran], transform.position, Quaternion.identity);
+
+        }
+        else
+        {
+            currentRound++;
+            foreach (GameObject spawner in spawnerList)
+            {
+                spawner.GetComponent<Spawner>().RunSpawn(spawnAmount,spawnMonsters);
+            }
+        }
+      
+    }
+}
